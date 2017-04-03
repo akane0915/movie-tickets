6b20065dd5c1060d27baa1597c499a9f913d30db
Asia Kane and Sean Pierce <asia@>
2017-04-03 16:10:48 -0700

    Create fullAdress prototype

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,9 @@ function Address(street, city, state) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
 
 
 // UI logic
@@ -62,7 +65,7 @@ $(document).ready(function(){
       $(".last-name").text(newContact.lastName);
       $("ul#addresses").text("");
       newContact.addresses.forEach(function(address){
-        $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
       });
     }); // Contact click function close
 
